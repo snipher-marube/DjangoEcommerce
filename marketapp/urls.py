16e5987bea7240d8dd5c2db88f2a5b0900d413a1
snipher <sniphermarube@gmail.com>
2022-10-21 09:00:12 +0300

    Checkout design

--- marketapp/urls.py
+++ marketapp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 
 from . import views
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('blog/', views.blog, name='blog'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
 ]
--- marketapp/views.py
+++ marketapp/views.py
@@ -2,3 +2,16 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'marketapp/home.html')
+
+def contact(request):
+    return render(request, 'marketapp/contact.html')
+
+def blog(request):
+    return render(request, 'marketapp/blog-single-sidebar.html')
+
+def cart(request):
+    return render(request, 'marketapp/cart.html')
+
+def checkout(request):
+    return render(request, 'marketapp/checkout.html')
+
